e6c1978ab01531925bd80c0fd1661370d34d0831
quangle-epay <quang.le@epay.vn>
2021-09-17 23:49:01 +0700

    redo module & package

--- code_along/hello.py
+++ code_along/hello.py
@@ -2,6 +2,12 @@ print("Hello class")
 print("The variable __name__ tells me which context this file is running in.")
 print("The value of __name__ is:", repr(__name__))
 
+class Query():
+    pass
+
+class Database():
+    pass
+
 def main():
     print("Hello in main!")
 
